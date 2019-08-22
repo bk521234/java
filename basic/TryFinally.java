47a75972076fcaecfe8156912ef537a64fea1374
salvidor <bryan.kielpinski52@gmail.com>
2019-08-21 19:15:01 -0600

    adding exception handling files.

---
+++ basic/TryFinally.java
@@ -0,0 +1,13 @@
+class TryFinally {
+    public static void main(String args[]){
+        try{
+            int d = 1;
+            int n =20;
+            int fraction = n/d;
+        } catch (ArithmeticException e) {
+            System.out.println("In the catch clock due to Exception = "+e);
+        } finally {
+            System.out.println("Inside the finally block");
+        }
+    }
+}
